c478ad97223668d692f82f3fbac2fd6822d79ff6
Mattis <mattismeeuwesse@gmail.com>
2021-11-07 19:24:10 +0100

    Configure JWT Middleware & Service injection

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,19 +1,39 @@
+using DAL;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Service;
 using System.Threading.Tasks;
+using Webshop.Middleware;
 
 namespace OnlineStore
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureFunctionsWorkerDefaults((IFunctionsWorkerApplicationBuilder Builder) => {
+                    Builder.UseNewtonsoftJson().UseMiddleware<JwtMiddleware>();
+                })
+                .ConfigureOpenApi()
+                .ConfigureServices(Configure)
                 .Build();
 
-            host.Run();
+            await host.RunAsync();
+        }
+
+        static void Configure(HostBuilderContext Builder, IServiceCollection Services)
+        {
+            Services.AddDbContext<OnlineStoreDBContext>();
+            Services.AddScoped<IUserService, UserService>();
+            Services.AddScoped<IAuthService, AuthService>();
+            Services.AddScoped<IProductService, ProductService>();
+            Services.AddScoped<IOrderService, OrderService>();
+            Services.AddScoped<IReviewService, ReviewService>();
         }
     }
 }
